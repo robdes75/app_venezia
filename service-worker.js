da89164f1beaa2aa8bfdb2865b317bfca9cc1eaf
robdes75 <robertodes75@gmail.com>
2026-01-13 15:18:44 +0100

    Refactor service worker to simplify install and activate
    Removed caching logic and simplified event listeners.

--- service-worker.js
+++ service-worker.js
@@ -1,31 +1,7 @@
-const CACHE_NAME = "venezia-pwa-v2";
-
-const FILES_TO_CACHE = [
-  "./",
-  "./index.html",
-  "./manifest.json",
-  "./data/punti.json"
-];
-
-self.addEventListener("install", e => {
+self.addEventListener("install", () => {
   self.skipWaiting();
-  e.waitUntil(
-    caches.open(CACHE_NAME).then(cache => cache.addAll(FILES_TO_CACHE))
-  );
 });
 
-self.addEventListener("activate", e => {
-  e.waitUntil(
-    caches.keys().then(keys =>
-      Promise.all(keys.map(k => {
-        if (k !== CACHE_NAME) return caches.delete(k);
-      }))
-    )
-  );
-});
-
-self.addEventListener("fetch", e => {
-  e.respondWith(
-    caches.match(e.request).then(r => r || fetch(e.request))
-  );
+self.addEventListener("activate", () => {
+  self.clients.claim();
 });
