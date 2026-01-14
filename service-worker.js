7a247d559f44e4cafea4b449ec1f185c8b87a407
robdes75 <robertodes75@gmail.com>
2026-01-14 12:33:40 +0100

    Enhance service worker activation to clear caches
    Clear all caches during service worker activation.

--- service-worker.js
+++ service-worker.js
@@ -2,6 +2,15 @@ self.addEventListener("install", () => {
   self.skipWaiting();
 });
 
-self.addEventListener("activate", () => {
+self.addEventListener("activate", (event) => {
+  event.waitUntil(
+    caches.keys().then(keys =>
+      Promise.all(keys.map(k => caches.delete(k)))
+    )
+  );
   self.clients.claim();
+});
+
+self.addEventListener("fetch", (event) => {
+  event.respondWith(fetch(event.request));
 });
